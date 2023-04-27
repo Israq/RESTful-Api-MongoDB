96736e944e1c223fb4ca65c660e564a62a9d6185
Syed Ragib Israq <ragib.israq9833@gmail.com>
2023-04-27 22:07:07 +0600

    mongodb || dotenv pkg || Routes

--- routes/posts.js
+++ routes/posts.js
@@ -2,10 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
+const Post = require('../models/Post');
+
 router.get('/', (req, res) => {
     res.send('The app is working on this page');
 });
 router.get('/specific', (req, res) => {
     res.send('The app is working on this page/specific');
 });
+
+
+
 module.exports = router;
